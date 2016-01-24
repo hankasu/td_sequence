dd111297fa6bfbecfb24d597348e67813383465c
Henry Colomb <Henry Colomb>
2016-01-24 11:46:22 -0700

    Reverted to sequence

--- main.py
+++ main.py
@@ -1,109 +1,118 @@
-# Put any initialization logic here.  The context object will be passed to
-# the other methods in your algorithm.
+#lesson 1 algorithm
+
 def initialize(context):
-    context.security = sid(37736) #UCO
-    context.setupPeriod = 13
-    context.SEQUENCE_COUNT = 9
-    context.COUNTDOWN_COUNT = 13
-    context.buy_setup_count = 0
-    context.sell_setup_count = 0
-    context.countdown_period = 0
-    context.recycle = 0
-    context.last_setup = None
-
-#check for a buy setup which is a close lower than the close
-#from 4 sessions previous
-def checkSetup(context):
-    #in the history dataframe the 0th index is the start of the history
-    set_history = history(context.setupPeriod, '1d', 'close_price')
-    period_high = history(context.setupPeriod, '1d', 'high')[context.security].max()
-    period_low = history(context.setupPeriod, '1d', 'low')[context.security].min()
-    sec_series = set_history[context.security]
-    setupCounter = 0
+    #initializ a list of context stocks
+    #context.uco = sid(37736) long oil
+    #context.sco = sid(37737) short oil
+    #context.boil = sid(41988) long natural gas
+    #context.kold = sid(41987) short natural gas
+    context.etf = sid(41988)
+    context.shortEtf = sid(41987)
     
-    #check the past 9 intervals
-    for idx in range(4, context.setupPeriod):
-        #buy setup security going down
-        if  sec_series[idx] <= sec_series[idx - 4]:
-            setupCounter -= 1
-        #sell setup going up
-        if sec_series[idx] >= sec_series[idx - 4]:
-            setupCounter += 1
-        
-    if setupCounter == context.SEQUENCE_COUNT and sec_series.min() > period_low:
-        return 'sell'
-    elif setupCounter == -context.SEQUENCE_COUNT and sec_series.max() < period_high:
-        return 'buy'
-    else:
-        return None
- 
-
-def checkCountdown(context):
-    close_series = history(context.countdown_period, '1d', 'close_price')[context.security]
-    high_series = history(context.countdown_period, '1d', 'high')[context.security]
-    low_series = history(context.countdown_period, '1d', 'low')[context.security]
-    
-    setup_counter = 0
-    
-    for idx in range(context.countdown_period):
-        if close_series[idx] <= low_series[idx - 2]:
-            setup_counter -= 1
-        if close_series[idx] >= high_series[idx - 2]:
-            setup_counter += 1
-    #return which setup based on the sign
-    if setup_counter == context.COUNTDOWN_COUNT:
-        return 'sell'
-    elif setup_counter == -context.COUNTDOWN_COUNT:
-        return 'buy'
-    else:
-        return None
-
-def reset_counters(context):
-    context.sell_setup_count = 0
-    context.buy_setup_count = 0
-    context.countdown_period = 0
-    
-    
-# Will be called on every trade event for the securities you specify. 
-def handle_data(context, data):
-    #check for the setup
-    setup_type = checkSetup(context)
-    
-    if last_setup == 'buy' or last_setup == 'sell':
-        context.recycle += 1
-        
-    if setup_type == 'buy':
-        log.info('Buy Setup Hit')
-        context.buy_setup_count += 1
-               
-    elif setup_type == 'sell':
-        log.info('Sell Setup Hit')
-        context.sell_setup_count += 1
+    #consecutive sequence counters
+    context.sell_seq = 1
+    context.buy_seq = 1
+    context.sell_cd = 1
+    context.buy_cd = 1
   
-    #check for trend cancellations
-    if (context.sell_setup_count > 1 or context.buy_setup_count > 1 or
-        context.sell_setup_count == 1 and context.buy_setup_count == 1):
-        reset_counters(context)
-        
-    #check if we are in a countdown phase
-    if context.sell_setup_count == 1 or context.buy_setup_count == 1:
-        context.countdown_period += 1
-        
-    #sell countdown
-    if (context.sell_setup_count == 1 and 
-        context.countdown_period >= context.COUNTDOWN_COUNT + 2):#15 intervals
-        setup_cd = checkCountdown(context)
-        if setup_cd == 'sell':
-            log.info('Sell Countdown Hit')
-            reset_counters(context)
+    #consecutive setup and countdown states
+    context.in_buy_setup = False
+    context.in_sell_setup = False
+    context.in_buy_countdown = False
+    context.in_sell_countdown = False
+
+def reset(context):
+    #consecutive sequence counters
+    context.buy_seq = 1
+    context.sell_seq = 1
     
-    #buy countdown
-    if (context.buy_setup_count == 1 and 
-        context.countdown_period >= context.COUNTDOWN_COUNT + 2):#15 intervals
-        setup_cd = checkCountdown(context)
-        if setup_cd == 'buy':
-            log.info('Buy Countdown Hit')
-            reset_counters(context)
+    #consecutive countdown counters
+    context.buy_cd = 1
+    context.sell_cd = 1
+    context.in_buy_setup = False
+    context.in_sell_setup = False
+    context.in_buy_countdown = False
+    context.in_sell_countdown = False
+
+#Will be called on every trade event for the securities you specify
+def handle_data(context, data):
     
-    #set the setup marker
-    context.last_setup = setup_type
+    #check if current close is higher or lower than the n-4 interval
+    close_history = history(5, '1d', 'close_price')
+    low_history = history(3, '1d', 'low')
+    high_history = history(3, '1d', 'high')
+    
+    #get the previous closing
+    #close from 4 previous sessions
+    past_close = close_history[context.etf][0]
+    current_close = close_history[context.etf][4]
+    
+    #get previous high and low
+    past_low = low_history[context.etf][0]
+    past_high =  high_history[context.etf][0]
+    
+    #increment the counters if close is lower or higher than close
+    #4 days ago
+    if current_close >= past_close:
+        context.sell_seq += 1
+        context.buy_seq = 1
+    else:
+        context.buy_seq += 1
+        context.sell_seq = 1   
+    
+    #reached the 9 sell setup
+    if context.sell_seq % 9 == 0:
+        log.info('Sell sequence hit')
+        #reset sequence check
+        context.sell_seq = 1
+        
+        #check for recycle
+        if context.in_sell_setup == True:
+            reset(context)
+        else:
+            context.in_sell_countdown = True
+            context.in_sell_setup = True
+            
+            #reset the buy setup and countdown
+            context.in_buy_setup = False
+            context.in_buy_countdown = False
+
+    if context.buy_seq % 9 == 0:
+        log.info('Buy sequence hit')
+        context.buy_seq = 1
+        
+        #check for recycle
+        if context.in_buy_setup == True:
+            reset(context)
+        else:
+            context.in_buy_countdown = True
+            context.in_buy_setup = True
+            
+            #reset the sell countdown and setup
+            context.in_sell_countdown = False
+            context.in_sell_setup = False
+
+    #Countdown phase
+    if context.in_buy_countdown == True:
+        #check previous lows and highs
+        if current_close <= past_low:
+            context.buy_cd += 1
+
+        #if we get to 13 sell signal
+        if context.buy_cd == 13:
+            log.info('Buying ETF')
+            order_percent(context.etf, 0.33)
+            #clear everything out
+            reset(context)
+
+    if context.in_sell_countdown == True:
+        if current_close >= past_high:
+            context.sell_cd += 1
+
+        #if we get to 13 sell signal
+        if context.sell_cd == 13:
+            log.info('Buying ultra short ETF')
+            order_percent(context.shortEtf, 0.33)
+            #clear everything out
+            reset(context)
+        
